9f4f8846725a38832ce65e7f5d9092607906b159
AndrewHamel111 <me@andrewhamel.ca>
2026-04-18 14:06:51 -0400

    Fixed user name not syncing

--- scripts/world.gd
+++ scripts/world.gd
@@ -28,6 +28,7 @@ func _ready() -> void:
 	menu_manager.join_pressed.connect(join_game)
 	menu_manager.pause_state_changed.connect(set_pause)
 	menu_manager.lobby.start_game.connect(start_game)
+	menu_manager.show_menu(Menu.MAIN)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_pressed("pause"):
@@ -87,10 +88,7 @@ func join_game(address: String) -> void:
 	multiplayer.multiplayer_peer = enet_peer
 
 @rpc("any_peer", "call_remote", "reliable")
-func _on_connected_to_host() -> void:
-	menu_manager.show_menu(Menu.LOBBY)
-	menu_manager.lobby.set_host_mode(false)
-	
+func _on_connected_to_host(server_state: int) -> void:
 	# set username
 	var peer := multiplayer.get_unique_id()
 	var my_user := get_node("Users/%d" % peer) as User
@@ -98,7 +96,10 @@ func _on_connected_to_host() -> void:
 		push_error("Failed to get user in _on_connected_to_host")
 	my_user.username = menu_manager.name_entry.text
 	
-	menu_manager.lobby.set_player_list(get_player_list())
+	if server_state as GameState == GameState.PRE:
+		menu_manager.show_menu(Menu.LOBBY)
+		menu_manager.lobby.set_host_mode(false)
+		menu_manager.lobby.set_player_list(get_player_list())
 
 func get_player_list() -> Array[User]:
 	var users: Array[User] = []
@@ -106,6 +107,10 @@ func get_player_list() -> Array[User]:
 		if user is User:
 			users.push_back(user as User)
 	return users
+
+@rpc("any_peer", "call_local", "reliable")
+func _player_list_changed() -> void:
+	menu_manager.lobby.set_player_list(get_player_list())
 
 @rpc("any_peer", "call_local", "reliable")
 func _prepare_client() -> void:
@@ -129,6 +134,7 @@ func _client_finished_loading(peer_id: int) -> void:
 		finally_start_game()
 
 func finally_start_game() -> void:
+	state = GameState.STARTED
 	for user in get_player_list():
 		add_player(user.get_name().to_int())
 	_on_start_game.rpc()
@@ -147,19 +153,19 @@ func _on_start_game() -> void:
 func _on_peer_connected(peer_id: int) -> void:
 	var new_user := UserScene.instantiate() as User
 	new_user.name = "%d" % peer_id
+	new_user.name_changed.connect(func()->void: _player_list_changed())
 	$Users.add_child(new_user)
+	
+	_on_connected_to_host.rpc_id(peer_id, state)
 	
 	if state == GameState.STARTED:
 		add_player(peer_id)
 	elif state == GameState.STARTING:
-		# TODO: idk, maybe we can tell the peer they can't connect while the game is starting?
-		# or maybe we just let their game hang for a second until everyone else has loaded, or
-		# do we just add them to the "loading queue"?
-		pass
-	else:
-		_on_connected_to_host.rpc_id(peer_id)
+		loading_players.push_back(peer_id)
+		_prepare_client.rpc_id(peer_id)
 	
-	menu_manager.lobby.set_player_list(get_player_list())
+	#_player_list_changed.rpc()
+	#get_tree().create_timer(0.5).timeout.connect(func() -> void: _player_list_changed.rpc())
 
 func _on_peer_disconnected(peer_id: int) -> void:
 	if state == GameState.STARTED:
@@ -206,3 +212,6 @@ func _on_player_death(player: Player) -> void:
 	var spawn := get_player_spawn()
 	player.position = spawn.position
 	player.rotation = spawn.rotation
+
+func _on_user_spawner_spawned(node: Node) -> void:
+	(node as User).name_changed.connect(func() -> void: _player_list_changed())
